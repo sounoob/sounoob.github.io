f703ec7273e022bd01a7787d9b203c720c4302e3
Flavio Sena <flavio.sena@i9xp.com.br>
2020-02-06 14:03:51 -0300

    Add formspree rollback

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,23 @@
----
----
+// ---
+// ---
 function send() {
     const xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://magic.sounoob.com.br/contato.php");
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            if(response.status == 200) {
-                window.location = '/contato/obrigado';
-            } else {
-                alert(response.msg);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                const response = JSON.parse(this.responseText);
+                if (response.status == 200) {
+                    window.location = '/contato/obrigado';
+                } else {
+                    alert(response.msg);
+                }
+            }else {
+                document.getElementById('contact').action = 'https://formspree.io/xoqdldoy';
+                document.getElementById('contact').submit()
             }
         }
     };
-    xhttp.send("name=" + document.getElementById("name").value + "&email=" + document.getElementById("email").value + "&msg=" + document.getElementById("msg").value + "");
+    xhttp.send("name=" + document.getElementById("name").value + "&email=" + document.getElementById("_replyto").value + "&msg=" + document.getElementById("message").value + "");
 }
